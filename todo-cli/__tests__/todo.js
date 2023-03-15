92ed9a74a6ecec5058589a57d46ba2f04fa4c929
Swathi-20110808 <125352568+Swathi-20110808@users.noreply.github.com>
2023-03-15 22:09:28 +0530

    Add files via upload

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,100 @@
+// Todo Test js
+const todoList = require("../todo");
+const { all, markAsComplete, add, overdue, dueLater, dueToday } = todoList();
+
+describe("Todo New Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Testing 1",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+    add({
+      title: "Do cleaning",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    add({
+      title: "Buy Bike",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+  });
+  test("add new todo", () => {
+    const todoItemsCounts = all.length;
+    add([
+      {
+        title: "testing the Over Due",
+        completed: false,
+        dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+          .toISOString()
+          .slice(0, 10),
+      },
+    ]);
+    expect(all.length).toBe(todoItemsCounts + 1);
+  });
+
+  test("mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+
+  test("check the retreival of Overdue", () => {
+    expect(
+      all.filter((item) => item.dueDate < new Date().toISOString().slice(0, 10))
+        .length
+    ).toBe(1);
+    add({
+      title: "Buy Electric car",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+    expect(
+      all.filter((item) => item.dueDate < new Date().toISOString().slice(0, 10))
+        .length
+    ).toBe(2);
+  });
+
+  test("check the retreival of due today", () => {
+    expect(
+      all.filter(
+        (item) => item.dueDate === new Date().toISOString().slice(0, 10)
+      ).length
+    ).toBe(1);
+    add({
+      title: "Buy milk",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    expect(
+      all.filter(
+        (item) => item.dueDate === new Date().toISOString().slice(0, 10)
+      ).length
+    ).toBe(2);
+  });
+});
+
+test("check retreival of Overdue", () => {
+  expect(
+    all.filter((item) => item.dueDate > new Date().toISOString().slice(0, 10))
+      .length
+  ).toBe(1);
+  add({
+    title: "Submit Work",
+    completed: false,
+    dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
+      .toISOString()
+      .slice(0, 10),
+  });
+  expect(
+    all.filter((item) => item.dueDate > new Date().toISOString().slice(0, 10))
+      .length
+  ).toBe(2);
+});
